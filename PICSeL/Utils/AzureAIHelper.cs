0fca4f2d48d8b2dc3e4e0350be014e2e7d98836c
Ayushi Agrawal <ayagrawa@microsoft.com>
2024-02-29 12:44:44 +0000

    Updated AzureAIHelper.cs

--- PICSeL/Utils/AzureAIHelper.cs
+++ PICSeL/Utils/AzureAIHelper.cs
@@ -23,7 +23,7 @@ namespace PICSeL.Utils
             return GetResponseFromOpenAI(message, createSummarizeRequest, lang)?.Choices?.FirstOrDefault()?.Message.content ?? string.Empty;
         }
 
-        public string GetVideoScript(string message, bool isQuery = false)
+        public string GetVideoScript(string message)
         {
             return GetResponseFromOpenAI(message, CreateVideoScript, lang)?.Choices?.FirstOrDefault()?.Message.content ?? string.Empty;
         }
